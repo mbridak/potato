53837bf80b5ed738c119a92685801c50912e90a9
mbridak <michael.bridak@gmail.com>
2021-05-13 12:24:59 -0700

    Icon Installer for binary

---
+++ install_icon/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource install --size 64 --context apps --mode user k6gte-potato.png k6gte-potato
+
+xdg-desktop-icon install k6gte-potato.desktop
+
+xdg-desktop-menu install k6gte-potato.desktop
+
---
+++ install_icon/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource uninstall --size 64 k6gte-potato
+
+xdg-desktop-icon uninstall k6gte-potato.desktop
+
+xdg-desktop-menu uninstall k6gte-potato.desktop
+
